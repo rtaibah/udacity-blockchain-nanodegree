9ab0aa2662aaaa3129baf84a0f79d13bc350c3ac
Rami Taibah <taibah@gmail.com>
2018-09-02 12:19:03 +0300

    improvements

--- blockchain-api/api.js
+++ blockchain-api/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const Blockchain = require('./simpleChain.js')
+const getBlock = require('./simpleChain').getBlock;
+const addBlock = require('./simpleChain').addBlock;
+const Block = require('./block')
 
 // App setup
 const app = express();
@@ -15,14 +17,18 @@ app.use(bodyParser.json({type: '*/*'}));
 
 // Routes
 app.get('/block/:block', (req, res, next) => {
-  let block = Blockchain.getBlock(req.params.block)
+  let block = getBlock(req.params.block)
   block.then(function(result) {
-    res.send(result);
+    res.send(JSON.parse(result));
   })
 });
 
-app.put('/post', (req, res, next) => {
-  let addBlock = Blockchain.addBlock(req)
+app.post('/block', (req, res) => {
+  console.log(req.body)
+  let add = addBlock(new Block(req.body))
+  add.then(function(result) {
+    res.send(result);
+  })
 });
 
 
