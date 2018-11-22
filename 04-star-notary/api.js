a63ec829d39dbdb57bd176729d20981d05c09eea
Rami Taibah <taibah@gmail.com>
2018-11-22 12:55:03 +0300

    Blockchain ID validation routine of the Rubric done

--- 04-star-notary/api.js
+++ 04-star-notary/api.js
@@ -2,9 +2,9 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const Blockchain = require('./simpleChain')
-const Block = require('./block')
-const StarValidation = require('./starValidation')
+const Blockchain = require('./simpleChain');
+const Block = require('./block');
+const StarValidation = require('./starValidation');
 
 // App setup
 const app = express();
@@ -13,51 +13,90 @@ const app = express();
 app.use(morgan('combined'));
 
 // Parse incoming requests
-app.use(bodyParser.json({type: '*/*'}));
+app.use(bodyParser.json({ type: '*/*' }));
 
 validateAddressParameter = async (req, res, next) => {
   try {
-    const starValidation = new StarValidation(req)
-    starValidation.validateAddressParameter()
-    next()
+    const starValidation = new StarValidation(req);
+    starValidation.validateAddressParameter();
+    next();
   } catch (error) {
     res.status(400).json({
       status: 400,
-      message: error.message
-    })
+      message: error.message,
+    });
   }
-}
+};
+
+validateSignatureParameter = async (req, res, next) => {
+  try {
+    const starValidation = new StarValidation(req);
+    starValidation.validateSignatureParameter();
+    next();
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      message: error.message,
+    });
+  }
+};
 
 // Routes
 app.get('/block/:block', (req, res, next) => {
-  let block = Blockchain.getBlock(req.params.block)
+  let block = Blockchain.getBlock(req.params.block);
   block.then(function(result) {
     res.send(JSON.parse(result));
-  })
+  });
 });
 
 app.post('/block', (req, res) => {
-  let add = Blockchain.addBlock(new Block(req.body))
+  let add = Blockchain.addBlock(new Block(req.body));
   add.then(function(result) {
     res.send(result);
-  })
+  });
 });
 
 app.post('/requestValidation', [validateAddressParameter], async (req, res) => {
-  const starValidation = new StarValidation(req)
-  const address = req.body.address
+  const starValidation = new StarValidation(req);
+  const address = req.body.address;
 
   try {
-    data = await starValidation.getPendingAddressRequest(address)
+    data = await starValidation.getPendingAddressRequest(address);
   } catch (error) {
-    data = await starValidation.saveNewRequestValidation(address)
+    data = await starValidation.saveNewRequestValidation(address);
   }
 
-  res.json(data)
-})
+  res.json(data);
+});
+
+app.post(
+  '/message-signature/validate',
+  [validateAddressParameter, validateSignatureParameter],
+  async (req, res) => {
+    const starValidation = new StarValidation(req);
+    const address = req.body.address;
+    const signature = req.body.signature;
+    try {
+      const response = await starValidation.validateSignature(
+        address,
+        signature,
+      );
+      if (response.registerStar) {
+        res.json(response);
+      } else {
+        res.status(401).json(response);
+      }
+    } catch (error) {
+      res.status(404).json({
+        status: 404,
+        message: error.message,
+      });
+    }
+  },
+);
 
 // Server setup
 const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 server.listen(port);
-console.log("Server listening on port ", port);
+console.log('Server listening on port ', port);
--- 04-star-notary/starValidation.js
+++ 04-star-notary/starValidation.js
@@ -1,4 +1,5 @@
 const db = require('level')('./data/star');
+const bitcoinMessage = require('bitcoinjs-message');
 
 class StarValidation {
   constructor(req) {
@@ -8,6 +9,13 @@ class StarValidation {
   validateAddressParameter() {
     if (!this.req.body.address) {
       throw new Error('No address provided. Please provide one.');
+    }
+    return true;
+  }
+
+  validateSignatureParameter() {
+    if (!this.req.body.signature) {
+      throw new Error('No signature provided. Please provide one');
     }
     return true;
   }
@@ -28,6 +36,57 @@ class StarValidation {
     return data;
   }
 
+  async validateSignature(address, signature) {
+    return new Promise((resolve, reject) => {
+      db.get(address, (err, value) => {
+        if (value === undefined) {
+          return reject(new Error('Not Found'));
+        } else if (err) {
+          return reject(err);
+        }
+        value = JSON.parse(value);
+        // Check if messageSignature is valid
+        if (value.messageSignature === 'valid') {
+          return resolve({
+            registerStar: true,
+            status: value,
+          });
+          // Check if registeration window has not expired
+        } else {
+          const underFiveMinutes = Date.now() - 5 * 60 * 1000;
+          const isExpired = value.requestTimeStamp < underFiveMinutes;
+          let isValid = false;
+
+          if (isExpired) {
+            value.validationWindow = 0;
+            value.messageSignature = 'Validation window expired';
+          } else {
+            // Check if message signature is valid
+            value.validationWindow = Math.floor(
+              (value.requestTimeStamp - underFiveMinutes) / 1000,
+            );
+            try {
+              isValid = bitcoinMessage.verify(
+                value.message,
+                address,
+                signature,
+              );
+            } catch (err) {
+              isValid = false;
+            }
+            value.messageSignature = isValid ? 'valid' : 'invalid';
+          }
+          db.put(address, JSON.stringify(value));
+
+          return resolve({
+            registerStar: !isExpired && isValid,
+            status: value,
+          });
+        }
+      });
+    });
+  }
+
   async getPendingAddressRequest(address) {
     return new Promise((resolve, reject) => {
       db.get(address, (err, value) => {
@@ -38,7 +97,6 @@ class StarValidation {
         }
 
         value = JSON.parse(value);
-
         const underFiveMinutes = Date.now() - 5 * 60 * 1000;
         const isExpired = value.requestTimeStamp < underFiveMinutes;
 
